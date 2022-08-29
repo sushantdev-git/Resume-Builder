7b5795b94da82b04a2c2d7b05be41890b491eb43
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-08-29 10:48:17 +0530

    added react router and two pageslanding page and editor page

---
+++ src/Pages/EditorPage/EditorPage.jsx
@@ -0,0 +1,10 @@
+import styles from './EditorPage.module.css';
+const EditorPage = () => {
+    return (
+        <div className={styles.Container}>
+            <h1>Editor Page</h1>
+        </div>
+    )
+}
+
+export default EditorPage;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import styles from './LandingPage.module.css';
+
+const LandingPage = () => {
+    return(
+        <div className={styles.Container}>
+            <h1>Landing Page</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
