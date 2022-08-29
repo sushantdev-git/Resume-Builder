d9d79017ecf1cd3718dafa3f5327230eb4e05f74
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-08-29 10:51:23 +0530

    deleted some files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
 import {Routes, Route} from 'react-router-dom';
 import LandingPage from './Pages/LandingPage/LandingPage';
 import EditorPage from './Pages/EditorPage/EditorPage';
