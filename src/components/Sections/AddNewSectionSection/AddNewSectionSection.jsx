c1dfb6a72050f89cd60aa3316d02e995b45b0bd5
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-09-02 14:32:02 +0530

    added moving icons of section

---
+++ src/components/Sections/AddNewSectionSection/AddNewSectionSection.jsx
@@ -0,0 +1,40 @@
+import styles from './style.module.css';
+
+import Section from '../Section';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBriefcase,
+  faPuzzlePiece,
+  faSchool,
+  faFire,
+} from "@fortawesome/free-solid-svg-icons";
+
+const AddNewSectionSection = () => {
+    return (
+        <Section name="Add Section">
+            <div className={styles.AddSectionButton}>
+              <FontAwesomeIcon icon={faPuzzlePiece} size="2x" color="#42A0E4" />
+              <h3>Custom Section</h3>
+            </div>
+            <div className={styles.AddSectionButton}>
+              <FontAwesomeIcon icon={faSchool} size="2x" color="#42A0E4" />
+              <h3>Education</h3>
+            </div>
+            <div className={styles.AddSectionButton}>
+              <FontAwesomeIcon icon={faBriefcase} size="2x" color="#42A0E4" />
+              <h3>Work Experience</h3>
+            </div>
+            <div className={styles.AddSectionButton}>
+              <FontAwesomeIcon icon={faFire} size="2x" color="#42A0E4" />
+              <h3>Skills</h3>
+            </div>
+            <div className={styles.AddSectionButton}>
+              <FontAwesomeIcon icon={faFire} size="2x" color="#42A0E4" />
+              <h3>Internship</h3>
+            </div>
+          </Section>
+    )
+}
+
+export default AddNewSectionSection;
---
+++ src/components/Sections/Section.jsx
@@ -0,0 +1,29 @@
+import IconButton from '../Buttons/IconButton/IconButton';
+import styles from './Section.module.css';
+
+import {
+    faArrowUp,
+    faTrash,
+    faArrowDown
+  } from "@fortawesome/free-solid-svg-icons";
+
+const Section = (props) => {
+    return (
+        <div className={styles.Section}>
+            <div className={styles.TitleAndButtons}>
+                <h2>{props.name}</h2>
+                <div className={styles.Buttons}>
+                    <IconButton icon={faArrowUp}/>
+                    <IconButton icon={faArrowDown}/>
+                    <IconButton icon={faTrash} color="red"/>
+                </div>
+            </div>
+            <div className={styles.Line}></div>
+            <div className={styles.SectionContents}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default Section;
