9fe7627a7b9bac9a4227b2ba65df5da43d70c962
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-09-15 22:57:12 +0530

    some fixing

--- src/components/EditorComponents/Sections/SkillsSection/SkillItem/SkillItem.jsx
+++ src/components/EditorComponents/Sections/SkillsSection/SkillItem/SkillItem.jsx
@@ -41,7 +41,7 @@ const SkillItem = ({ data, index, skillSize }) => {
           dispatch(
             ModalActions.openModal({
               callBack: () => dispatch(SkillsActions.deleteSkill({ index })),
-              content: `Do you want to delete Education/${data.degree}?`,
+              content: `Do you want to delete Education/${skillName}?`,
             })
           )
         }
