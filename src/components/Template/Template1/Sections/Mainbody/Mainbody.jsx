09d057e32a300c2f6f5aab0bc673bcde9b460faa
rachit-sindhu <rachitsindhu242@gmail.com>
2022-09-18 12:16:50 +0530

    Mostly template 1 finished

--- src/components/Template/Template1/Sections/Mainbody/Mainbody.jsx
+++ src/components/Template/Template1/Sections/Mainbody/Mainbody.jsx
@@ -2,13 +2,49 @@ import styles from './Mainbody.module.css'
 import Profile from '../../Sub-Sections/Profile/Profile'
 import ExperienceSection from '../../Sub-Sections/ExperienceSection/ExperienceSection'
 import EducationSection from '../../Sub-Sections/EducationSection/EducationSection'
+import { useSelector } from 'react-redux'
+import * as IDS from '../../../../../store/SectionIDS'
+
+const mapping={};
+mapping[IDS.PERSONAL_DETAILS_SECTION] = (idx) => (
+<Profile
+    sectionDetails={{
+    index: idx,
+    ID: IDS.PERSONAL_DETAILS_SECTION,
+    }}
+/>
+);
+mapping[IDS.EDUCATION_SECTION] = (idx) => (
+<EducationSection
+    sectionDetails={{
+    index: idx,
+    ID: IDS.EDUCATION_SECTION,
+    }}
+/>
+);
+mapping[IDS.EXPERIENCE_SECTION] = (idx) => (
+<ExperienceSection
+    sectionDetails={{
+    index: idx,
+    ID: IDS.EXPERIENCE_SECTION,
+    }}
+/>
+);
+  
 
 const Mainbody = () => {
+    const positions = useSelector((state) => state.position.position);
+    const sections= positions.filter((section) => {
+        return section==IDS.PERSONAL_DETAILS_SECTION ||
+               section==IDS.EDUCATION_SECTION ||
+               section==IDS.EXPERIENCE_SECTION;
+    })    
+    
     return (
         <div className={styles.Container}>
-            <Profile></Profile>
-            <ExperienceSection></ExperienceSection>
-            <EducationSection></EducationSection>
+            {sections.map((id, ind) => {
+                return mapping[id](ind);
+            })}
         </div>
         
     )
