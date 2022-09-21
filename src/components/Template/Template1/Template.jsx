81a6366672ab537f000ae58099036d4b6ee2c435
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-09-21 21:50:38 +0530

    some layout fixing for small screens

--- src/components/Template/Template1/Template.jsx
+++ src/components/Template/Template1/Template.jsx
@@ -1,15 +1,20 @@
-import Sidebar from './Sections/Sidebar/Sidebar'
-import Mainbody from './Sections/Mainbody/Mainbody'
-import styles from './Template.module.css'
-import React from 'react';
+import Sidebar from "./Sections/Sidebar/Sidebar";
+import Mainbody from "./Sections/Mainbody/Mainbody";
+import styles from "./Template.module.css";
+import React from "react";
 
 const Template = React.forwardRef((props, ref) => {
-    return (
-        <div className={styles.Container} ref={ref}>
-            <Sidebar></Sidebar>
-            <Mainbody></Mainbody>
-        </div>
-    );
+  return (
+    <div className={styles.Container} ref={ref}>
+      <style type="text/css" media="print">
+        {"\
+            @page { size: portrait; }\
+        "}
+      </style>
+      <Sidebar></Sidebar>
+      <Mainbody></Mainbody>
+    </div>
+  );
 });
 
-export default Template
+export default Template;
